ef1639d4d58f20d6dd87223683f2c38886281c5f
pelonpelon <github@xlfag.com>
2013-06-08 18:36:41 -0500

    Moving to sass/compass/jade/coffee --again

---
+++ godaddy/wp/wp-content/themes/foundation/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package foundation
+ */
+?>
+
+	</div><!-- #main -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<?php do_action( 'foundation_credits' ); ?>
+			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'foundation' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'foundation' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'foundation' ), 'foundation', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
