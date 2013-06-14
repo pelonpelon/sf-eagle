4416fa624514ffbecd69dbdcf55db2a351403dc2
pelonpelon <github@xlfag.com>
2013-06-13 22:40:43 -0500

    working. cleaned main directory. using jade + sass

--- godaddy/wp/wp-content/themes/foundation/config.rb
+++
@@ -1,9 +0,0 @@
-http_path = "/"
-css_dir = "/"
-sass_dir = "scss"
-images_dir = "images"
-javascripts_dir = "js"
-
-output_style = :nested
-environment = :development
-
--- godaddy/wp/wp-includes/jslive.php
+++
@@ -1,5 +0,0 @@
-$length = ob_get_length();
-$last_modified = date ("F d Y H:i:s", getlastmod());
-header("Content-Length: $length");
-header("Last-Modified: $last_modified GMT time");
-
